31c53a9e9c889bf56afec0a462027a40a941ebfa
yardanrizki <yardanrizki@gmail.com>
2026-06-07 17:55:10 +0700

    Initial commit CI3 absensi QR code system

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.1
+    </div>
+    <strong> &copy; 2022 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE. | Repost by <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'>Yardanrizki.</a>
+	
+</footer>
